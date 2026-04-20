f69705a150923f684f8c1a78157a37cf1fa19217
Anish Anandan <anishanandan12@gmail.com>
2026-04-20 11:19:12 +0530

    GitHub Actions deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/kidtrace/",
   css: {
     modules: {
       localsConvention: "camelCaseOnly",
@@ -12,4 +13,4 @@ export default defineConfig({
   build: {
     sourcemap: true,
   },
-})
+});
